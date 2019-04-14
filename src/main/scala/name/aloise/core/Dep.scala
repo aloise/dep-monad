bd3d2605966a11198586e39fbd2e2abebfb625cd
Ihor <erwaht@gmail.com>
2019-04-14 11:03:07 +0200

    evolution

--- src/main/scala/name/aloise/core/Dep.scala
+++ src/main/scala/name/aloise/core/Dep.scala
@@ -14,17 +14,20 @@ trait Dep2[A] { self =>
   type Inj <: HList // all injected and captured params - ie all "A" types
   type EnvWoInj <: HList
 
-  protected def runWithInjected(environment: Env, injected: Inj): A
+  protected def runWithInjected(environment: EnvWoInj, injected: Inj): A
 
-  def run(env: EnvWoInj)(implicit eq: HNil =:= Inj, eq2: EnvWoInj =:= Env): A =
-    runWithInjected(eq2(env), eq(HNil))
+  def run(implicit envNil: HNil =:= EnvWoInj, injNil: HNil =:= Inj): A =
+    runWithInjected(envNil(HNil), injNil(HNil))
 
-  def map[B, Out <: HList](f: A => B)(implicit en: FilterNot.Aux[Env, B, Out]): Dep2.Aux[Env, B :: Inj, B] = new Dep2[B] {
+  def run(env: EnvWoInj)(implicit eq: HNil =:= Inj): A =
+    runWithInjected(env, eq(HNil))
+
+  def map[B, EnvWoInjOut <: HList](f: A => B)(implicit filter: FilterNot.Aux[Env, B, EnvWoInjOut]): Dep2.Aux[Env, B :: Inj, EnvWoInjOut, B] = new Dep2[B] {
     override type Env = self.Env
     override type Inj = B :: self.Inj
-    override type EnvWoInj = Out
+    override type EnvWoInj = EnvWoInjOut
 
-    override protected def runWithInjected(environment: Env, injected: Inj): B = {
+    override protected def runWithInjected(environment: EnvWoInj, injected: Inj): B = {
       val parentRun = self.runWithInjected(environment, injected.tail)
       val mapped = f(parentRun)
       mapped
@@ -34,29 +37,64 @@ trait Dep2[A] { self =>
   def flatMap[
                 Env2 <: HList,
                 Inj2 <: HList,
+                EwoI <: HList,
                 CombinedEnv <: HList,
                 CombinedEnvMinusInjected <: HList,
                 B]
-            (f: A => Dep2.Aux[Env2, Inj2, B])
+            (f: A => Dep2.Aux[Env2, Inj2, EwoI, B])
             (implicit union: Union.Aux[Env, Env2, CombinedEnv],
              injUnique: NotContainsConstraint[self.Inj, B],
-
-            ): Dep2.Aux[Env2, B :: Inj, CombinedEnvMinusInjected] = new Dep2[B] {
+             diff: Diff.Aux[CombinedEnv, B :: self.Inj, CombinedEnvMinusInjected]
+            ): Dep2.Aux[CombinedEnv, B :: Inj, CombinedEnvMinusInjected, B] = new Dep2[B] {
     override type Env = CombinedEnv
     override type Inj = B :: self.Inj
-    override type EnvWoInj = this.type
+    override type EnvWoInj = CombinedEnvMinusInjected
 
-    override protected def runWithInjected(environment: Env, injected: Inj): B = ???
+    override protected def runWithInjected(environment: EnvWoInj, injected: Inj): B = ???
   }
 }
 
 object Dep2 {
-  type Aux[E, I, A1] = Dep2[A1]{
+  type Aux[E, I, EwoI, A1] = Dep2[A1]{
     type Env = E
     type Inj = I
+    type EnvWoInj = EwoI
+  }
+
+  def pure[A](f: => A): Aux[HNil, A :: HNil, HNil, A] = new Dep2[A] {
+    override type Env = HNil
+    override type Inj = A :: HNil
+    override type EnvWoInj = HNil
+
+    override protected def runWithInjected(environment: EnvWoInj, injected: Inj): A = f
+  }
+
+  def reader[A,B](f: A => B): Aux[A :: HNil, B :: HNil, A :: HNil, B] = new Dep2[B] {
+    override type Env = A :: HNil
+    override type Inj = B :: HNil
+    override type EnvWoInj = A :: HNil
+
+    override protected def runWithInjected(environment: EnvWoInj, injected: Inj): B = f(environment.head)
   }
 }
 
 object Test {
+
+  import Dep2._
+
+  val ad: Aux[HNil, Int :: HNil, HNil, Int] = pure(1)
+  val bd: Aux[HNil, String :: HNil, HNil, String] = pure("str")
+
+  val x = ad.flatMap(a => bd).map(x => x.length)
+
+//  val dep = for {
+//    a <- ad
+//    b <- bd
+//  } yield a + b.length
+//
+//  def main(args: Array[String]): Unit = {
+//    println(dep.run)
+//  }
+
 
 }

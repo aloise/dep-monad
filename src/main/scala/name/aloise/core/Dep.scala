55e81f6a8519d618dc5ad6e2e7d3bcfd28f79f8f
Ihor M <erwaht@gmail.com>
2020-07-26 17:45:27 +0200

    formatting

--- src/main/scala/name/aloise/core/Dep.scala
+++ src/main/scala/name/aloise/core/Dep.scala
@@ -1,33 +1,29 @@
 package name.aloise.core
 
-import shapeless.HNil
-
 private[core] object hset {
 
   import scala.language.implicitConversions
   import shapeless.{::, =:!=, DepFn1, DepFn2, HList, HNil, NotContainsConstraint}
 
-  final class HListSetOps[L <: HList](l : L){
-    def :+:[E](e: E)(implicit nc: NotContainsConstraint[L, E]) = ::(e, l)
+  final class HListSetOps[L <: HList](l: L) {
+    def :+:[E](e: E)(implicit nc: NotContainsConstraint[L, E]): E :: L = ::(e, l)
   }
 
   implicit def hlistSetOps[L <: HList](l: L): HListSetOps[L] = new HListSetOps(l)
-
-  implicit def notContainsConstraintHNilType[A]: NotContainsConstraint[HNil.type, A] =
-    new NotContainsConstraint[HNil.type, A] {}
 
   trait SetUnion[L <: HList, R <: HList] extends DepFn2[L, R] {
     type Out <: HList
   }
 
   trait LowPrioritySetUnion {
-    type Aux[L1 <: HList, R1 <: HList, Out1 <: HList] = SetUnion[L1, R1]{
+    type Aux[L1 <: HList, R1 <: HList, Out1 <: HList] = SetUnion[L1, R1] {
       type Out = Out1
     }
 
     // let ∅ ∪ M = M
     implicit def hsetEmptyUnionHNil[M <: HList]: Aux[HNil, M, M] = new SetUnion[HNil, M] {
       override type Out = M
+
       def apply(l: HNil, m: M): Out = m
     }
 
@@ -37,7 +33,8 @@ private[core] object hset {
     // let ∅ ∪ M = M
     implicit def hsetEmptyUnion[M <: HList]: Aux[HNil.type, M, M] = new SetUnion[HNil.type, M] {
       override type Out = M
-      def apply(l: HNil.type , m: M): Out = m
+
+      def apply(l: HNil.type, m: M): Out = m
     }
   }
 
@@ -52,6 +49,7 @@ private[core] object hset {
     ): Aux[H :: T, M, U] =
       new SetUnion[H :: T, M] {
         override type Out = U
+
         override def apply(t: H :: T, m: M): Out =
           unionTail(t.tail, ::(t.head, remove(m)))
       }
@@ -62,7 +60,7 @@ private[core] object hset {
   }
 
   trait Priority0SetExclude {
-    type Aux[L1 <: HList, R1 <: HList, Out1 <: HList] = SetExclude[L1, R1]{
+    type Aux[L1 <: HList, R1 <: HList, Out1 <: HList] = SetExclude[L1, R1] {
       type Out = Out1
     }
 
@@ -77,6 +75,7 @@ private[core] object hset {
   trait Priority1SetExclude extends Priority0SetExclude {
     implicit def emptyExclude[L <: HList]: Aux[L, HNil, L] = new SetExclude[L, HNil] {
       override type Out = L
+
       override def apply(t: L, u: HNil): L = t
     }
   }
@@ -89,6 +88,7 @@ private[core] object hset {
     ): SetExclude.Aux[L, RH :: RT, Out2] =
       new SetExclude[L, RH :: RT] {
         override type Out = Out2
+
         override def apply(t: L, u: RH :: RT): Out2 = exclude2(remove(t), u.tail)
       }
   }
@@ -98,9 +98,9 @@ private[core] object hset {
   }
 
   trait LowPrioritySetRemove {
-    type Aux[L1 <: HList, U, Out1 <: HList] = SetRemove[L1, U] { type Out = Out1 }
+    type Aux[L1 <: HList, U, Out1 <: HList] = SetRemove[L1, U] {type Out = Out1}
 
-    implicit def hEmptyRemoveHnil[U]: SetRemove.Aux[HNil , U, HNil] = new SetRemove[HNil , U] {
+    implicit def hEmptyRemoveHnil[U]: SetRemove.Aux[HNil, U, HNil] = new SetRemove[HNil, U] {
       override type Out = HNil
 
       override def apply(t: Out): HNil = HNil
@@ -109,7 +109,7 @@ private[core] object hset {
   }
 
   trait MediumPrioritySetRemove extends LowPrioritySetRemove {
-    implicit def hEmptyRemove[U]: SetRemove.Aux[HNil.type , U, HNil.type] = new SetRemove[HNil.type, U] {
+    implicit def hEmptyRemove[U]: SetRemove.Aux[HNil.type, U, HNil.type] = new SetRemove[HNil.type, U] {
       override type Out = HNil.type
 
       override def apply(t: Out): HNil.type = HNil
@@ -121,6 +121,7 @@ private[core] object hset {
 
     implicit def removeFromHList[H, T <: HList]: SetRemove.Aux[H :: T, H, T] = new SetRemove[H :: T, H] {
       type Out = T
+
       override def apply(t: H :: T): T = t.tail
     }
 
@@ -131,12 +132,15 @@ private[core] object hset {
     ): SetRemove.Aux[H :: T, U, H :: Out1] =
       new SetRemove[H :: T, U] {
         type Out = H :: Out1
+
         override def apply(t: H :: T): Out = t.head :: rm(t.tail)
       }
   }
+
 }
 
-trait Dep[A] { self =>
+trait Dep[A] {
+  self =>
 
   import hset._
 
@@ -166,25 +170,23 @@ trait Dep[A] { self =>
     override type Inj = B :: self.Inj
 
     override protected def runWithEnv(environment: Env): (B, Inj) = {
-
       val (parentRun, inj) = self.runWithEnv(environment)
-
       val mapped = f(parentRun)
       (mapped, mapped :+: inj)
     }
   }
 
   def flatMap[
-  Env2 <: HList,
-  Inj2 <: HSet,
-  CombinedInj <: HSet,
-  CombinedEnv <: HSet,
-  CombinedEnvMinusInjected <: HSet,
-  EnvWithInjected <: HSet,
-  EnvReconstructed <: HSet,
-  CombinedEnvWithInject <: HSet,
-  Env2Reconstructed <: HSet,
-  B]
+    Env2 <: HList,
+    Inj2 <: HSet,
+    CombinedInj <: HSet,
+    CombinedEnv <: HSet,
+    CombinedEnvMinusInjected <: HSet,
+    EnvWithInjected <: HSet,
+    EnvReconstructed <: HSet,
+    CombinedEnvWithInject <: HSet,
+    Env2Reconstructed <: HSet,
+    B]
   (f: A => Dep.Aux[Env2, Inj2, B])
   (implicit injectedCombined: SetUnion.Aux[Inj, Inj2, CombinedInj],
    combinedEnv: SetUnion.Aux[self.Env, Env2, CombinedEnv],
@@ -212,9 +214,10 @@ trait Dep[A] { self =>
 }
 
 object Dep {
+
   import shapeless.{HNil, ::}
 
-  type Aux[E, I, A1] = Dep[A1]{
+  type Aux[E, I, A1] = Dep[A1] {
     type Env = E
     type Inj = I
   }
@@ -229,7 +232,7 @@ object Dep {
     }
   }
 
-  def reader[A,B](f: A => B): Aux[A :: HNil, B :: HNil, B] = new Dep[B] {
+  def reader[A, B](f: A => B): Aux[A :: HNil, B :: HNil, B] = new Dep[B] {
     override type Env = A :: HNil
     override type Inj = B :: HNil
 
@@ -277,21 +280,32 @@ object Test2 {
 
   // inputs
   case class A1(i: Int)
+
   case class A2(i: Int)
+
   case class A3(i: Int)
+
   case class A4(i: Int)
+
   case class A5(i: Int)
 
   // outputs
   case class B1(i: Int)
+
   case class B2(i: Int)
+
   case class B3(i: Int)
+
   case class B4(i: Int)
+
   case class B5(i: Int)
+
   case class B6(s: String)
+
   case class B8(str: String)
 
   case class Final(i: Int)
+
   case class AnotherFinal(i: Int)
 
   case class Dep2B1(i: Int)
@@ -300,28 +314,28 @@ object Test2 {
   val dep = for {
     b1 <- pure(B1(5))
     _ <- pure(B2(15))
-    _ <- reader{(str: String, str2: B1) => str.length }
-    _ <- reader{(sameStr: String, b2: B2, b3: B3) => B4(1 + sameStr.length) }
-    _ <- reader{(str: String, str2: B1) => str.length }
-    _ <- reader{(sameStr3: String, b2: B2, b3: B3) => B4(4) }
-    _ <- reader{(str: String, str2: B1) => str.length }
-    _ <- reader{(sameStr4: String, b2: B2, b3: B3) => B5(5) }
-    _ <- reader{b: B5 => B6("test")}
-    b8 <- reader{ _:B2 => B8("aloise2") }
-  } yield Final(b1.i*2*b8.str.length)
+    _ <- reader { (str: String, str2: B1) => str.length }
+    _ <- reader { (sameStr: String, b2: B2, b3: B3) => B4(1 + sameStr.length) }
+    _ <- reader { (str: String, str2: B1) => str.length }
+    _ <- reader { (sameStr3: String, b2: B2, b3: B3) => B4(4) }
+    _ <- reader { (str: String, str2: B1) => str.length }
+    _ <- reader { (sameStr4: String, b2: B2, b3: B3) => B5(5) }
+    _ <- reader { b: B5 => B6("test") }
+    b8 <- reader { _: B2 => B8("aloise2") }
+  } yield Final(b1.i * 2 * b8.str.length)
 
   val dep2 = for {
-    len <- reader{(str: String, str2: B1) => Dep2B1(str.length) }
-    len2 = len.i*2
-    _ <- reader{b1: B1 => B2(b1.i)}
-    _ <- reader{(sameStr4: String, b2: B2, b3: B3) => B5(5*2 + sameStr4.length + b3.i) }
-    fn <- reader{ f: Final => f.i * 2L}
-  } yield AnotherFinal(len2+fn.toInt)
+    len <- reader { (str: String, str2: B1) => Dep2B1(str.length) }
+    len2 = len.i * 2
+    _ <- reader { b1: B1 => B2(b1.i) }
+    _ <- reader { (sameStr4: String, b2: B2, b3: B3) => B5(5 * 2 + sameStr4.length + b3.i) }
+    fn <- reader { f: Final => f.i * 2L }
+  } yield AnotherFinal(len2 + fn.toInt)
 
   val bigDep = for {
     d0 <- dep
     d1 <- dep2
-  } yield d0.i*d1.i
+  } yield d0.i * d1.i
 
   def main(args: Array[String]): Unit = {
 
